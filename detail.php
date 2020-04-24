dcf837a88b0a29dedcf00bbd53ad7e7714caaa66
Medr3x <leonardo@meemba.com.ar>
2020-04-24 09:33:07 -0300

    first commmit

---
+++ detail.php
@@ -0,0 +1,124 @@
+<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/6.0.0/normalize.min.css">
+    <link rel="stylesheet" href="./assets/style.css">
+    <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+
+    <title>Tienda e-commerce</title>
+</head>
+<style type="text/css">
+button.mercadopago-button {
+  background-color: #2D3277;
+}
+</style>
+<body>
+    <?php
+    require ('vendor/autoload.php');
+
+    // $_clientId = "8196777983571350";
+    // $_secretId = "qe5ICWOHsP0dIhCzKwj9i99xS5UH4sGQ";
+
+    // MercadoPago\SDK::setClientId($_clientId);
+    // MercadoPago\SDK::setClientSecret($_secretId);
+
+    // $_setPublicKey = "APP_USR-a83913d5-e583-4556-8c19-d2773746b430";
+    $_setAccessToken = "APP_USR-6317427424180639-090914-5c508e1b02a34fcce879a999574cf5c9-469485398";
+
+    // MercadoPago\SDK::setPublicKey($_setPublicKey);
+    MercadoPago\SDK::setAccessToken($_setAccessToken);
+
+    $preference = new MercadoPago\Preference();
+      
+    # Building an item
+
+    $preference->external_reference = "ABCD1234";
+
+    $preference->notification_url = "https://integracion-mp.herokuapp.com/notifications.php";
+
+    $preference->payment_methods  = [
+                                        'default_installments' => 6,    # 6 Cuota
+                                        'excluded_payment_types' => [ 
+                                            ['id' => 'amex'],         # American Express
+                                            ['id' => 'atm'],            # tampoco deposito
+                                        ]
+                                    ];
+
+    $item = new MercadoPago\Item();
+    $item->id               = $_GET['id'];
+    $item->title            = $_GET['title']; 
+    $item->description      = $_GET['description']; 
+    $item->picture_url      = $_GET['img'];
+    $item->quantity         = $_GET['unit'];
+    $item->unit_price       = $_GET['price'];
+
+    $preference->items = [$item];
+
+    //Datos del Usuario 
+
+    $payer = new MercadoPago\Payer();
+
+    $payer->name = "Lalo";
+    $payer->surname = "Landa";
+    $payer->email = "test_user_63274575@testuser.com";
+
+    $payer->identification = array(
+        "area_code" => "011",
+        "number" => "2222-3333"
+    );
+
+    $payer->identification = array(
+        "type" => "DNI",
+        "number" => "22333444"
+    );
+
+    $payer->address = array(
+        "street_name" => "Falsa",
+        "street_number" => 123,
+        "zip_code" => "1111"
+    );
+    # Return the HTML code for button
+    $preference->back_urls = array(
+                    "success" => "https://integracion-mp.herokuapp.com/success.php",
+                    "failure" => "https://integracion-mp.herokuapp.com/failure.php",
+                    "pending" => "https://integracion-mp.herokuapp.com/pending.php"
+                );
+
+    $preference->auto_return = "approved";
+    $preference->payer = $payer;
+
+    $preference->save(); # Save the preference and send the HTTP Request to create
+    ?>
+
+    <div class="wrap">
+        <center><h1>Tienda e-commerce</h1></center>
+            <div class="row">
+            <div class="col-sm-3">
+            <img src="<?php echo $_GET['img'] ?>" class="img-item" style="width: 200px;height: 260px; margin-top: 30px;"></img>
+            </div>
+            <div class="col-sm-9" style="margin-top: 50px">
+                <h4> <?php echo $_GET['title'] ?> </h4>
+                <h4>Cantidad: <?php echo $_GET['unit'] ?></h4>
+                <h4>Precio: $ <?php echo $_GET['price'] ?></h4>
+                <!-- <a href="<?php echo $preference->init_point; ?>">Pagar con Mercado Pago</a> -->
+                <form action="/detail.php" method="POST">
+
+                  <script
+                   src="https://www.mercadopago.com.ar/integrations/v1/web-payment-checkout.js"
+                   data-preference-id="<?php echo $preference->id; ?>" data-button-label="Pagar la compra" data-elements-color="#2D3277" >
+
+                  </script>
+                </form>
+            </div>
+            </div>
+    </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,199 @@
+<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/6.0.0/normalize.min.css">
+    <link rel="stylesheet" href="./assets/style.css">
+    <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+
+    <title>Tienda e-commerce</title>
+</head>
+<body>
+<div class="wrap">
+	<center><h1>Tienda e-commerce</h1></center>
+    <div class="menu">
+        <div class="mini-menu">
+        	<center style="padding: 10px; background: #dadada"><span style="color: #333; font-size: 15px; padding: 10px">Smartphones</span></center>
+            <ul>
+		        <li><a href="#">Apple</a></li>
+		        <li><a href="#">LG</a></li>
+		        <li><a href="#">Motorola</a></li>
+		        <li><a href="#">Samsung</a></li>
+		        <li><a href="#">Huawei</a></li>
+		        <li><a href="#">Sony</a></li>
+		    </ul>
+        </div>
+    </div>
+
+    <div class="items">
+        
+  <div class="items">
+        <div data-price="15000" class="item form-post" style=" cursor:pointer; cursor: hand">
+        	<form action="/detail.php" method="get">
+            	<img src="./assets/samsung-galaxy-s9-xxl.jpg" alt="samsung" class="img-item"></img>
+                <input type="hidden" name="img" value="./assets/samsung-galaxy-s9-xxl.jpg">
+                <input type="hidden" name="title" value="Samsung Galaxy S9">
+                <input type="hidden" name="price" value="15000">
+                <input type="hidden" name="unit" value="1">
+                <input type="hidden" name="description" value="Dispositivo móvil de Tienda e-commerce">
+                <input type="hidden" name="id" value="1234">
+                <div class="info">
+                    <h3>Samsung Galaxy S9</h3>
+                    <h5>$15.000</h5>
+                </div>
+            </form>
+        </div>
+        <div data-price="12000" class="item form-post" style=" cursor:pointer; cursor: hand">
+        	<form action="/detail.php" method="get">
+            	<img src="./assets/l6g6.jpg" alt="samsung" class="img-item"></img>
+                <input type="hidden" name="img" value="./assets/l6g6.jpg">
+                <input type="hidden" name="title" value="LG G6">
+                <input type="hidden" name="price" value="12000">
+                <input type="hidden" name="unit" value="1">
+                <input type="hidden" name="description" value="Dispositivo móvil de Tienda e-commerce">
+                <input type="hidden" name="id" value="1234">
+                <div class="info">
+                    <h3>LG G6</h3>
+                    <h5>$12.000</h5>
+                </div>
+            </form>
+        </div>
+        <div data-price="18000" class="item form-post" style=" cursor:pointer; cursor: hand">
+        	<form action="/detail.php" method="get">
+            	<img src="./assets/u_10168742.jpg" alt="samsung" class="img-item"></img>
+                <input type="hidden" name="img" value="./assets/u_10168742.jpg">
+                <input type="hidden" name="title" value="iPhone 8">
+                <input type="hidden" name="price" value="18000">
+                <input type="hidden" name="unit" value="1">
+                <input type="hidden" name="description" value="Dispositivo móvil de Tienda e-commerce">
+                <input type="hidden" name="id" value="1234">
+                <div class="info">
+                    <h3>iPhone 8</h3>
+                    <h5>$18.000</h5>
+                </div>
+            </form>
+        </div>
+        <div data-price="16000" class="item form-post" style=" cursor:pointer; cursor: hand">
+        	<form action="/detail.php" method="get">
+            	<img src="./assets/motorola-moto-g5-plus-1.jpg" alt="samsung" class="img-item"></img>
+                <input type="hidden" name="img" value="./assets/motorola-moto-g5-plus-1.jpg">
+                <input type="hidden" name="title" value="Motorola G5">
+                <input type="hidden" name="price" value="16000">
+                <input type="hidden" name="unit" value="1">
+                <input type="hidden" name="description" value="Dispositivo móvil de Tienda e-commerce">
+                <input type="hidden" name="id" value="1234">
+                <div class="info">
+                    <h3>Motorola G5</h3>
+                    <h5>$16.000</h5>
+                </div>
+            </form>
+        </div>
+        <div data-price="10000" class="item form-post" style=" cursor:pointer; cursor: hand">
+        	<form action="/detail.php" method="get">
+            	<img src="./assets/motorola-moto-g4-3.jpg" alt="samsung" class="img-item"></img>
+                <input type="hidden" name="img" value="./assets/motorola-moto-g4-3.jpg">
+                <input type="hidden" name="title" value="Moto G4">
+                <input type="hidden" name="price" value="10000">
+                <input type="hidden" name="unit" value="1">
+                <input type="hidden" name="description" value="Dispositivo móvil de Tienda e-commerce">
+                <input type="hidden" name="id" value="1234">
+                <div class="info">
+                    <h3>Moto G4</h3>
+                    <h5>$10.000</h5>
+                </div>
+            </form>
+        </div>
+        <div data-price="13000" class="item form-post" style=" cursor:pointer; cursor: hand">
+        	<form action="/detail.php" method="get">
+            	<img src="./assets/003.jpg" alt="samsung" class="img-item"></img>
+                <input type="hidden" name="img" value="./assets/003.jpg">
+                <input type="hidden" name="title" value="Sony Xperia XZ2">
+                <input type="hidden" name="price" value="13000">
+                <input type="hidden" name="unit" value="1">
+                <input type="hidden" name="description" value="Dispositivo móvil de Tienda e-commerce">
+                <input type="hidden" name="id" value="1234">
+                <div class="info">
+                    <h3>Sony Xperia XZ2</h3>
+                    <h5>$13.000</h5>
+                </div>
+            </form>
+        </div>
+    </div>
+    </div>
+</div>
+</body>
+
+    <!--<script
+  src="https://code.jquery.com/jquery-3.2.1.min.js"
+  integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+  crossorigin="anonymous"></script>-->
+
+
+
+
+
+    <!--Menu-->
+  <script type="text/javascript">
+    $(document).ready(function () {
+        $(".sub > a").click(function() {
+            var ul = $(this).next(),
+                    clone = ul.clone().css({"height":"auto"}).appendTo(".mini-menu"),
+                    height = ul.css("height") === "0px" ? ul[0].scrollHeight + "px" : "0px";
+            clone.remove();
+            ul.animate({"height":height});
+            return false;
+        });
+           $('.mini-menu > ul > li > a').click(function(){
+           $('.sub a').removeClass('active');
+           $(this).addClass('active');
+        }),
+           $('.sub ul li a').click(function(){
+           $('.sub ul li a').removeClass('active');
+           $(this).addClass('active');
+        });
+    });
+</script>
+<script src="script.js" ></script>
+</html>
+<script>
+    $( function() {
+    $( "#slider-range" ).slider({
+      range: true,
+      min: 0,
+      max: 1000,
+      values: [ 190, 728 ],
+      slide: function( event, ui ) {
+        $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+         var mi = ui.values[0];
+              var mx = ui.values[1];
+              filterSystem(mi, mx);
+      }
+    });
+    $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
+      " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+  } );
+
+
+  function filterSystem(minPrice, maxPrice) {
+      $(".items div.item").hide().filter(function () {
+          var price = parseInt($(this).data("price"), 10);
+          return price >= minPrice && price <= maxPrice;
+      }).show();
+  }
+  
+//   $( "#slider-range" ).on( "slidechange", function( event, ui ) {
+//     console.log(ui.value);
+// } );
+</script>
+<script type="text/javascript">
+	$(document).on('click','.form-post',function(){
+	    $(this).children('form').submit();
+	});
+</script>
